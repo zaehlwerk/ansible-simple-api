f909032d9d570e8232bad9259c3482aa5963c13e
Gregor Giesen <giesen@zaehlwerk.net>
2018-07-26 11:25:56 +0200

    Lazy import; example in README

--- README.rst
+++ README.rst
@@ -2,3 +2,10 @@
 Ansible Simple API
 ==================
 
+This is very simple wrapper around Ansible's API with sensible defaults.
+
+Here is an example:
+>>> from ansible_simple_api import Ansible
+>>> ansible = Ansible(connection='local', inventory='localhost,')
+>>> ansible.run('localhost', 'debug', msg="Hello Ansible Simple API")
+{localhost: {'msg': 'Hello Ansible Simple API', '_ansible_verbose_always': True, '_ansible_no_log': False, 'changed': False}}
--- lib/ansible_simple_api/__init__.py
+++ lib/ansible_simple_api/__init__.py
@@ -18,4 +18,14 @@
 # along with Ansible Simple API. If not, see <http://www.gnu.org/licenses/>.#
 """ansible_simple_api/__init__.py: Ansible Simple API"""
 
-from .api import Ansible
+def Ansible(*args, **kwargs):
+    """Unfortunately any import from Ansible takes some time (~1s) and
+    also creates (via import of ansible.constants) a temporary directory
+    which has to be cleaned up. Therefore we suspend importing and wrap
+    `Ansible` into a function which imports and calls it.
+
+    With PEP 562 (Python 3.7) this can be replaced with
+    module's __getattr__ """
+
+    from .api import Ansible
+    return Ansible(*args, **kwargs)
