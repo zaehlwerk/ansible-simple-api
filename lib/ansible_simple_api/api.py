9da464cc8295f314091eb877aee19be50a895d74
Gregor Giesen <giesen@zaehlwerk.net>
2018-07-26 13:02:45 +0200

    added `run_role` and `serial` keyword

--- lib/ansible_simple_api/api.py
+++ lib/ansible_simple_api/api.py
@@ -100,26 +100,39 @@ class Ansible:
         )
         self.results = result_callback.results
 
-    def run(self, hosts: str, module: str, _arg=None, **kwargs):
+    def run(self, hosts, module, _arg=None, *, serial=None, **kwargs):
         """Runs a single action on hosts"""
         results = self.run_play(
             None, hosts, tasks=[
                 dict(action=dict(
                     module=module,
-                    args=_arg if _arg else kwargs))])
+                    args=_arg if _arg else kwargs))], serial=serial)
         return dict((host, host_results[-1][1])
                     for host, host_results in results.items())
 
+    def run_role(self, hosts: str, role: str, serial=None, **kwargs):
+        """Runs a role on hosts with `kwargs` as additional variables"""
+        role = dict(role=role)
+        role.update(kwargs)
+        return self.run_play(None, hosts, roles=[role], serial=serial)
+
     def run_play(self, name: str, hosts: str, tasks=[], roles=[],
-            gather_facts: bool=False):
+                 gather_facts: bool=None, serial=None, **kwargs):
         """runs a play on hosts"""
-        play = Play.load(
-            dict(
+        play_dict = dict(
                 name=name,
                 hosts=hosts,
                 tasks=tasks,
-                roles=roles,
-                gather_facts='yes' if gather_facts else 'no'),
+                roles=roles)
+        if roles or gather_facts:
+            play_dict['gather_facts'] = 'yes'
+        else:
+            play_dict['gather_facts'] = 'no'
+        if serial:
+            play_dict['serial'] = serial
+        play_dict.update(kwargs)
+        play = Play.load(
+            play_dict,
             variable_manager=self.variable_manager, loader=self.loader)
         self.tqm.run(play)
         return self.results
